091946f7911e28652b7904792d5706c9a0c68e27
TeenFire01 <109087623+TeenFire01@users.noreply.github.com>
2025-05-24 21:47:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,16 @@ dotenv.load_dotenv()
 
 
 class Settings(BaseSettings):
-    BOT_TOKEN: str = Field("", env="BOT_TOKEN")
-    API_ID: int = Field(0, env="API_ID")
-    API_HASH: str = Field("", env="API_HASH")
+    BOT_TOKEN: str = Field("", env="6506281957:AAFCfX0fRiwT_G8UyrPr1Ubv9ETOmhlNq8Y")
+    API_ID: int = Field(0, env="16156788")
+    API_HASH: str = Field("", env="8153259ab0f78c9d6e5103e6a927d68b")
     MONGO_URI: str = Field(
         "",
-        env="MONGO_URI",
+        env="mongodb+srv://savio:<savio>@cluster0.ukm9uqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     )
     DATABASE_NAME: str = Field("FileDrawerBot", env="DATABASE_NAME")
-    STORAGE_CHANNEL_ID: int = Field(0, env="STORAGE_CHANNEL_ID")
-    ADMIN_USER_IDS: list[int] = Field([], env="ADMIN_USER_IDS")
+    STORAGE_CHANNEL_ID: int = Field(0, env="-1001902086657")
+    ADMIN_USER_IDS: list[int] = Field([], env="862880955")
 
 
 settings = Settings()
